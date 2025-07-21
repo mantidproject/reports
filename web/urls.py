a8c41e39893d3217a3034f4b4b839b697215ce18
Waruna Wickramasingha <waruna.wickramasingha@stfc.ac.uk>
2025-07-21 18:15:52 +0100

    corrected import modules for url

--- web/urls.py
+++ web/urls.py
@@ -1,13 +1,11 @@
-from django.conf.urls import include, url
-from django.urls import path
-
+from django.urls import include, path
 from django.contrib import admin
-admin.autodiscover()
-
 from django.views.generic.base import RedirectView
 
 import report
 import services
+
+admin.autodiscover()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
